c4a228df56539b6f2d0e5710db58b58b52a0cf9f
Huacai Chen <chenhuacai@loongson.cn>
2021-02-06 17:18:08 +0800

    LoongArch: Add writecombine support for drm
    LoongArch maintains cache coherency in hardware, but its WUC attribute
    (Weak-ordered UnCached, which is similar to WC) is out of the scope of
    cache coherency machanism. This means WUC can only used for write-only
    memory regions.
    
    Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>

--- drivers/gpu/drm/ttm/ttm_bo_util.c
+++ drivers/gpu/drm/ttm/ttm_bo_util.c
@@ -347,7 +347,7 @@ pgprot_t ttm_io_prot(struct ttm_buffer_object *bo, struct ttm_resource *res,
 		tmp = pgprot_noncached(tmp);
 #endif
 #if defined(__ia64__) || defined(__arm__) || defined(__aarch64__) || \
-    defined(__powerpc__) || defined(__mips__)
+    defined(__powerpc__) || defined(__mips__) || defined(__loongarch__)
 	if (caching == ttm_write_combined)
 		tmp = pgprot_writecombine(tmp);
 	else
